eef46969c67ee6a11aea83b8195b1f9f54fb56ff
HazemAlMelli <hazemalmile7@gmail.com>
2025-04-02 01:10:50 +0200

    finish

--- app.js
+++ app.js
@@ -7,16 +7,44 @@ const port = 3000;
 const users = [];
 
 app.use(express.json());
+
 //Register User API
 app.post("/register", async (req, res) => {
   try {
     const { email, password } = req.body;
     const findUser = users.find((data) => email == data.email);
     if (findUser) {
-      res.status(400).send("You Are Already Access");
+      res.status(400).send("Wrong Email or Password");
     }
-  } catch (err) {
-    res.status(500).send({ message: err.message });
+    //Hash Password
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = { email, password: hashedPassword };
+    users.push(user);
+    console.log(users);
+    res.status(201).send("User Created Successfully");
+  } catch {
+    res.status(500).send({ error: "Internal Server Error" });
+  }
+});
+
+//Login User API
+app.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const findUser = users.find((data) => email == data.email);
+    // Check if user exists
+    if (!findUser) {
+      return res.status(400).send("Wrong Email or Password");
+    }
+    //Compare Password
+    const passwordMatch = await bcrypt.compare(password, findUser.password);
+    if (!passwordMatch) {
+      return res.status(400).send("Wrong Email or Password");
+    } else {
+      res.status(200).send("Login Successful");
+    }
+  } catch (error) {
+    res.status(500).send({ error: "Internal Server Error" });
   }
 });
 
